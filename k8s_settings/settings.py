eb11c6453d9098c4022f05ef8702c45e906bb07e
Andy Gallagher <andy.gallagher@theguardian.com>
2020-06-10 16:53:09 +0100

    ignoring settings.py

--- k8s_settings/settings.py
+++
@@ -1,4 +0,0 @@
-### This file is imported from the main settings.py, at the end. It functions to over-ride any
-### any application settings from the main file and is placed in a subdirectory so it can be easily
-### over-ridden with a K8s config map
-
