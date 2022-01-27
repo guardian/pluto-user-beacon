379d6d48b4300108254aad926ccad14adbdf20e0
andy_gallagher <andy.gallagher@theguardian.com>
2022-01-27 18:16:51 +0000

    Update JWT auth module to bring it inline with deliverables, loading the key from a file

--- userbeacon/jwt_auth_backend.py
+++ userbeacon/jwt_auth_backend.py
@@ -1,5 +1,5 @@
-import jwt
 import logging
+import jwt
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.contrib.auth import PermissionDenied, authenticate
@@ -14,45 +14,47 @@ logger = logging.getLogger(__name__)
 class JwtAuth(object):
     @staticmethod
     def load_public_key_from_cert():
-        cert_raw = settings.JWT_VALIDATION_KEY.encode("ASCII")
-        cert = load_pem_x509_certificate(cert_raw, default_backend())
-        return cert.public_key()
+        try:
+            with open(settings.JWT_CERTIFICATE_PATH, "r") as certfile:
+                cert_raw = certfile.read().encode("ASCII")
+                cert = load_pem_x509_certificate(cert_raw, default_backend())
+                return cert.public_key()
+        except Exception as e:
+            logger.error('Could not read certificate: ' + str(e))
+            raise
 
     def __init__(self):
         self._public_key = self.load_public_key_from_cert()
 
+    @staticmethod
+    def _extract_username(claims):
+        username = claims.get("username")   #adfs uses this one
+        if username is None:
+            username = claims.get("preferred_username") #keycloak uses this one
+        if username is None:
+            logger.warning("Could not get username from claims set, expect problems")
+        return username
+
     def authenticate(self, request, **credentials):
         token = credentials.get("token", None)
         if token:
-            logger.info("JwtAuth got token {0}".format(token))
+            logger.debug("JwtAuth got token {0}".format(token))
             try:
                 decoded = jwt.decode(token,
                                      key=self._public_key,
                                      algorithms=["RS256"],
                                      audience=getattr(settings, "JWT_EXPECTED_AUDIENCE", None),
                                      issuer=getattr(settings, "JWT_EXPECTED_ISSUER", None))
-                logger.info("JwtAuth success")
-
-                if hasattr(settings,"ADMIN_CLAIM_NAME"):
-                    if decoded.get(settings.ADMIN_CLAIM_NAME, None) is not None:
-                        user_is_admin = True
-                    else:
-                        user_is_admin = False
-                else:
-                    user_is_admin = False
-
-                actual_user = decoded.get("preferred_username") #keycloak prefers this
-                if actual_user is None:
-                    actual_user = decoded.get("username")       #adfs prefers this
+                logger.debug("JwtAuth success")
 
                 return User(
-                    username=actual_user,
+                    username=self._extract_username(decoded),
                     first_name=decoded.get("first_name"),
                     last_name=decoded.get("family_name"),
                     email=decoded.get("email"),
-                    is_staff=user_is_admin,
+                    is_staff=True,
                     is_active=True,
-                    is_superuser=user_is_admin
+                    is_superuser=True   #until we have groups added in to the JWT claim
                 )
             except jwt.exceptions.DecodeError as e:
                 logger.error("Could not decode provided JWT: {0}".format(e))
@@ -60,7 +62,7 @@ class JwtAuth(object):
             except jwt.exceptions.ExpiredSignatureError:
                 logger.error("Token signature has expired")
             except jwt.exceptions.InvalidAudienceError:
-                logger.error("Token was for another audience")
+                logger.error("Token was for another audience: {0}".format())
             except Exception as e:
                 logger.error("Unexpected error decoding JWT: {0}".format(traceback.format_exc(e)))
         raise PermissionDenied()
@@ -84,25 +86,3 @@ class JwtRestAuth(BaseAuthentication):
                 raise AuthenticationFailed
         else:
             return None #authentication not attempted
-
-
-class JwtAuthMixin(object):
-    """
-    this class is a Django-view compatible mixin class
-    which calls out to our authentication backend via django.authenticate
-    """
-    def dispatch(self, request, *args, **kwargs):
-        auth_header = request.META.get("HTTP_AUTHORIZATION", None)
-        auth_cookie = request.COOKIES.get("adfstest-auth", None)
-
-        user_model = None
-        if isinstance(auth_header, str) and auth_header.startswith("Bearer "):
-            user_model = authenticate(request, token=auth_header[7:])
-        elif auth_cookie:
-            user_model = authenticate(request, token=auth_cookie)
-
-        if user_model:
-            request.user = user_model
-            return super(JwtAuthMixin, self).dispatch(request, *args, **kwargs)
-        else:
-            raise PermissionDenied("no auth header")
