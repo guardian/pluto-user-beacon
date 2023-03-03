16c6715ac003f84f0667c1789cdd694a7679dc4d
Dave Allison <david.allison@theguardian.com>
2023-03-03 15:37:15 +0000

    Remove code which changed the user name

--- userbeacon/jwt_auth_backend.py
+++ userbeacon/jwt_auth_backend.py
@@ -36,8 +36,6 @@ class JwtAuth(object):
             username = claims.get("preferred_username") #keycloak and Azure AD use this
         if username is None:
             logger.warning("Could not get username from claims set, expect problems")
-        if username is not None and "@" in username:
-                username = username.split("@")[0].replace(".", "_")
         return username
 
     def authenticate(self, request, **credentials):
